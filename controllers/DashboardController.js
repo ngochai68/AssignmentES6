639b4a1ddf1f0e1c0a10b81193e984bd25a4a732
ngochai68 <90449421+ngochai68@users.noreply.github.com>
2023-06-13 02:33:18 +0700

    update

--- controllers/DashboardController.js
+++ controllers/DashboardController.js
@@ -1,68 +1,107 @@
-const Movie = require("../models/movieModel");
-const Genre = require("../models/genreModel");
-const Area = require("../models/areaModel");
+const Movie = require('../models/movieModel');
+const Genre = require('../models/genreModel');
+const Area = require('../models/areaModel');
+const fs = require('fs');
+const path = require('path');
+
 
 const dashboardController = {
-  // ...các phương thức khác
+  getDashBoard: async (req, res) => {
+    try {
+      const movies = await Movie.getAllMovies();
+      res.render('admin/dashboard', { movies });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+    }
+  },
 
   getAllMovies: async (req, res) => {
     try {
       const movies = await Movie.getAllMovies();
-      const genres = await Genre.getAllGenres();
-      const areas = await Area.getAllAreas();
-      res.render("admin/movies", { movies, genres, areas });
+      res.render('admin/movie-manage', { movies });
     } catch (error) {
       console.error(error);
-      res.status(500).send("Internal Server Error");
+      res.status(500).send('Internal Server Error');
     }
   },
 
-  getMovieDetails: async (req, res) => {
+  getAddMovieForm: async (req, res) => {
+    try {
+      const movies = await Movie.getAllMovies();
+      const genres = await Genre.getAllGenres();
+      const areas = await Area.getAllAreas();
+      res.render('admin/add-movie', { movies, genres, areas });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+    }
+  },
+  addMovie: async (req, res) => {
+    try {
+      const movieData = req.body;
+
+      const { img1Url, img2Url } = req.files;
+
+      // Chuyển đổi các trường dữ liệu thành số
+      movieData.totalEpisodes = parseInt(movieData.totalEpisodes);
+      movieData.areaId = parseInt(movieData.areaId);
+      movieData.genreIds = movieData.genreIds.map(id => parseInt(id));
+      movieData.rating = parseFloat(movieData.rating);
+      movieData.score = parseFloat(movieData.score);
+      movieData.numberReviews = parseInt(movieData.numberReviews);
+      movieData.yearManufacture = parseInt(movieData.yearManufacture);
+
+      // Lưu trữ ảnh 1
+      const img1Path = path.join(__dirname, '../public/img/', img1Url[0].originalname);
+      fs.writeFileSync(img1Path, img1Url[0].buffer);
+      movieData.img1Url = `/img/${img1Url[0].originalname}`;
+
+      // Lưu trữ ảnh 2
+      const img2Path = path.join(__dirname, '../public/img/', img2Url[0].originalname);
+      fs.writeFileSync(img2Path, img2Url[0].buffer);
+      movieData.img2Url = `/img/${img2Url[0].originalname}`;
+
+      // Gọi phương thức createMovie từ model
+      const newMovie = await Movie.createMovie(movieData);
+      
+      res.redirect('/dashboard/movies');
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+    }
+  },
+  getEditMovieForm: async (req, res) => {
     try {
       const movieId = req.params.id;
       const movie = await Movie.getMovieById(movieId);
-      const genres = await Genre.getAllGenres();
-      const areas = await Area.getAllAreas();
-      res.render("admin/movie-details", { movie, genres, areas });
+      res.render('dashboard/edit-movie', { movie });
     } catch (error) {
       console.error(error);
-      res.status(500).send("Internal Server Error");
+      res.status(500).send('Internal Server Error');
     }
   },
-
-  createMovie: async (req, res) => {
-    try {
-      const movieData = req.body; // Lấy dữ liệu phim từ request body
-      const newMovie = await Movie.createMovie(movieData);
-      res.redirect("/dashboard/movies"); // Chuyển hướng sau khi tạo phim thành công
-    } catch (error) {
-      console.error(error);
-      res.status(500).send("Internal Server Error");
-    }
-  },
-
-  updateMovie: async (req, res) => {
+  editMovie: async (req, res) => {
     try {
       const movieId = req.params.id;
-      const movieData = req.body; // Lấy dữ liệu cập nhật từ request body
-      const updatedMovie = await Movie.updateMovie(movieId, movieData);
-      res.redirect(`/dashboard/movies/${movieId}`); // Chuyển hướng sau khi cập nhật phim thành công
+      const movieData = req.body;
+      await Movie.updateMovie(movieId, movieData);
+      res.redirect('/dashboard');
     } catch (error) {
       console.error(error);
-      res.status(500).send("Internal Server Error");
+      res.status(500).send('Internal Server Error');
     }
   },
-
   deleteMovie: async (req, res) => {
     try {
       const movieId = req.params.id;
-      const deletedMovie = await Movie.deleteMovie(movieId);
-      res.redirect("/dashboard/movies"); // Chuyển hướng sau khi xóa phim thành công
+      await Movie.deleteMovie(movieId);
+      res.redirect('/dashboard');
     } catch (error) {
       console.error(error);
-      res.status(500).send("Internal Server Error");
+      res.status(500).send('Internal Server Error');
     }
-  }
+  },
 };
 
 module.exports = dashboardController;
--- routes/dashboardRoute.js
+++ routes/dashboardRoute.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const dashboardController = require('../controllers/DashboardController');
+const multer = require('multer');
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
+router.get('/', dashboardController.getDashBoard);
 router.get('/movies', dashboardController.getAllMovies);
-router.get('/movies/:id', dashboardController.getMovieDetails);
-router.post('/movies/create', dashboardController.createMovie);
-router.post('/movies/update/:id', dashboardController.updateMovie);
-router.post('/movies/:id', dashboardController.deleteMovie);
+router.get('/add-movie', dashboardController.getAddMovieForm);
+router.post('/add-movie', upload.fields([{ name: 'img1Url', maxCount: 1 }, { name: 'img2Url', maxCount: 1 }]), dashboardController.addMovie);
+router.get('/edit-movie/:id', dashboardController.getEditMovieForm);
+router.put('/edit-movie/:id', dashboardController.editMovie);
+router.delete('/delete-movie/:id', dashboardController.deleteMovie);
+router.get('/img/:filename', (req, res) => {
+    const filename = req.params.filename;
+    const imgPath = path.join(__dirname, '../public/img/', filename);
+    res.sendFile(imgPath);
+  });
+  
+
 
 module.exports = router;
